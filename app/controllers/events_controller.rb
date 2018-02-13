0fc87e97055e5e368b3a0f55409c0e58854b1950
mjwilhite <mjwilhite1s@gmail.com>
2018-02-12 18:36:15 -0600

    First commit app creation and view controllers

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,5 @@
+class EventsController < ApplicationController
+	def index
+		@events = ["Carbondale Farmer's Market", "Mattie Jo's Glass Gardens party", "Carbondale Farmer's Market", "Jackson Farmer's Market"]
+ 	end
+end
