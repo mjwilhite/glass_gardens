63c80028ed9d0df61d72bebfb5a8023f271980a3
MJ <mjwilhite1s@gmail.com>
2018-03-08 22:16:43 -0600

    Form editing and Edit links

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,15 @@ class EventsController < ApplicationController
 	def show
 		@event = Event.find(params[:id])
 	end
+
+	def edit
+		@event = Event.find(params[:id])
+	end
+
+	def update
+		@event = Event.find(params[:id])
+		event_params = params.require(:event).permit(:name, :description, :starts_at, :location, :price)
+		@event.update(event_params)
+			redirect_to @event
+	end
 end
